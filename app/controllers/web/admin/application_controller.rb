e247a6aecaad5882dba36433a42467c20a5e31e4
Andrey Kumanyaev <me@zzet.org>
2012-11-17 00:59:16 +0400

    add admin template

--- app/controllers/web/admin/application_controller.rb
+++ app/controllers/web/admin/application_controller.rb
@@ -1,5 +1,5 @@
 class Web::Admin::ApplicationController < Web::ApplicationController
-  #layout 'web/admin/application'
+  layout 'web/admin/application'
 
   #helper_method :user_signed_in?, :current_user
 end
--- app/controllers/web/admin/welcome_controller.rb
+++ app/controllers/web/admin/welcome_controller.rb
@@ -1,4 +1,4 @@
-class Web::Admin::WelcomeController < ApplicationController
+class Web::Admin::WelcomeController < Web::Admin::ProtectedApplicationController
   def index
   end
 end
