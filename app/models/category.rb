db390ead2aa67a300d8bb48e8799c4fef84c1c8a
Andrey Kumanyaev <me@zzet.org>
2012-11-17 18:26:14 +0400

    categories and tags relations

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,6 +3,19 @@ class Category < ActiveRecord::Base
 
   validates :name, presence: true
 
-  has_many :post_categories, dependent: :destroy, class_name: Post::Category
-  has_many :posts, through: :post_categories
+  has_many :article_categories,   dependent: :destroy, class_name: Article::Category
+  has_many :biography_categories, dependent: :destroy, class_name: Biography::Category
+  has_many :gallery_categories,   dependent: :destroy, class_name: Gallery::Category
+  has_many :interview_categories, dependent: :destroy, class_name: Interview::Category
+  has_many :news_categories,      dependent: :destroy, class_name: News::Category
+  has_many :post_categories,      dependent: :destroy, class_name: Post::Category
+  has_many :video_categories,     dependent: :destroy, class_name: Video::Category
+
+  has_many :articles,     through: :article_categories
+  has_many :biographies,  through: :biography_categories
+  has_many :galleries,    through: :gallery_categories
+  has_many :interviews,   through: :interview_categories
+  has_many :news,         through: :news_categories
+  has_many :posts,        through: :post_categories
+  has_many :videos,       through: :video_categories
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,6 +3,19 @@ class Tag < ActiveRecord::Base
 
   validates :name, presence: true
 
-  has_many :post_tags, dependent: :destroy, class_name: Post::Tag
-  has_many :posts, through: :post_tags
+  has_many :article_tags,   dependent: :destroy, class_name: Article::Tag
+  has_many :biography_tags, dependent: :destroy, class_name: Biography::Tag
+  has_many :gallery_tags,   dependent: :destroy, class_name: Gallery::Tag
+  has_many :interview_tags, dependent: :destroy, class_name: Interview::Tag
+  has_many :news_tags,      dependent: :destroy, class_name: News::Tag
+  has_many :post_tags,      dependent: :destroy, class_name: Post::Tag
+  has_many :video_tags,     dependent: :destroy, class_name: Video::Tag
+
+  has_many :articles,       through: :article_tags
+  has_many :biographies,    through: :biography_tags
+  has_many :galleries,      through: :gallery_tags
+  has_many :interviews,     through: :interview_tags
+  has_many :news,           through: :news_tags
+  has_many :posts,          through: :post_tags
+  has_many :videos,         through: :video_tags
 end
